fc08a6baf7f1045ba8defbcdf3647f6c3e82fceb
rezamf17 <rezayuyara@gmail.com>
2021-11-07 15:21:31 +0700

    arrow function

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -3,10 +3,9 @@ function DataSource(onSuccess, onFailed) {
     this.onFailed = onFailed;
 }
 
-DataSource.prototype.searchClub = function (keyword) {
-    const filteredClubs = clubs.filter(function (club) {
-        return club.name.toUpperCase().includes(keyword.toUpperCase());
-    });
+DataSource.prototype.searchClub = (keyword) => {
+    const filteredClubs = clubs.filter((club) =>  lub.name.toUpperCase().includes(keyword.toUpperCase()));
+       
 
     if (filteredClubs.length) {
         this.onSuccess(filteredClubs);
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,17 +1,16 @@
-const main = function () {
+const main = () => {
     const searchElement = document.querySelector("#searchElement");
     const buttonSearchElement = document.querySelector("#searchButtonElement");
     const clubListElement = document.querySelector("#clubList");
 
-    const onButtonSearchClicked = function () {
+    const onButtonSearchClicked = () => {
         const dataSource = new DataSource(renderResult, fallbackResult);
         dataSource.searchClub(searchElement.value);
     };
 
-    const renderResult = function (results) {
+    const renderResult = results => {
         clubListElement.innerHTML = "";
-        results.forEach(function (club) {
-
+        results.forEach((club) => {
 
             // ({name, fanArt, description} = club);
             // or
@@ -28,7 +27,7 @@ const main = function () {
             clubListElement.appendChild(clubElement);
         })
     };
-    const fallbackResult = function (message) {
+    const fallbackResult = message => {
         clubListElement.innerHTML = "";
         clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
     };
