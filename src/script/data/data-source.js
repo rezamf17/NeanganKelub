4a9edef850254f18223813ff0ae581a7c15f120c
rezamf17 <rezayuyara@gmail.com>
2021-11-07 19:21:49 +0700

    class

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,15 +1,17 @@
-function DataSource(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-}
+class DataSource {
+    constructor(onSuccess, onFailed){
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
+    }
 
-DataSource.prototype.searchClub = (keyword) => {
-    const filteredClubs = clubs.filter((club) =>  lub.name.toUpperCase().includes(keyword.toUpperCase()));
+
+searchClub(keyword) {
+    const filteredClubs = clubs.filter((club) =>  club.name.toUpperCase().includes(keyword.toUpperCase()));
        
-
     if (filteredClubs.length) {
         this.onSuccess(filteredClubs);
     } else {
         this.onFailed(`${keyword} is not find`);
     }
-};
+    }
+}
