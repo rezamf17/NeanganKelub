4f93ac30e25890e44d50dbd9793821ae2318b9e8
rezamf17 <rezayuyara@gmail.com>
2021-11-08 19:02:05 +0700

    promise, callback, and another hard definition in js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,17 +1,14 @@
+
 class DataSource {
-    constructor(onSuccess, onFailed){
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }
-
-
-searchClub(keyword) {
-    const filteredClubs = clubs.filter((club) =>  club.name.toUpperCase().includes(keyword.toUpperCase()));
-       
-    if (filteredClubs.length) {
-        this.onSuccess(filteredClubs);
-    } else {
-        this.onFailed(`${keyword} is not find`);
-    }
-    }
+   static searchClub(keyword) {
+       return new Promise((resolve, reject) => {
+           const filteredClubs = clubs.filter((club) =>  club.name.toUpperCase().includes(keyword.toUpperCase()));
+              
+           if (filteredClubs.length) {
+               resolve(filteredClubs)
+           } else {
+               reject(`${keyword} is not find`);
+           }
+           })
+       } 
 }
