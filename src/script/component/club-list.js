bb6a5126105b4e968a108db056121e7a9cb3f1d9
rezamf17 <rezayuyara@gmail.com>
2021-11-10 17:57:07 +0700

    shadow dom

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,24 +1,42 @@
 import "./club-item.js"
 
 class ClubList extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+    
     set clubs(clubs) {
         this._clubs = clubs
         this.render()
     }
 
+    renderError(message) {
+        clubListElement.shadowDOM.innerHTML = "";
+        clubListElement.shadowDOM.innerHTML += `
+        <style>
+        .placeholder {
+            font-weight: lighter;
+            color: rgba(0,0,0,0.5);
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+         }
+          
+        </style>
+        <h2 class="placeholder">${message}</h2>`
+    }
     render() {
         this._clubs.forEach(club => {
-            this.innerHTML = ""
+            this.shadowDOM.innerHTML = ""
             const clubElementItem = document.createElement('club-item')
             clubElementItem.club = club
-            this.appendChild(clubElementItem)
+            this.shadowDOM.appendChild(clubElementItem)
         })
     }
 
-    renderError(message) {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
-    }
 }
 
 customElements.define('club-list', ClubList)
